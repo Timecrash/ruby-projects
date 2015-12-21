e37dda41658481401a3402d380e22333efa54718
Matt Velez <mzvelez75@gmail.com>
2015-12-21 16:07:47 -0500

    Update merge_sort.rb

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,26 +1,23 @@
 def merge_sort(ary)
   return ary if ary.length == 1
   mid = ary.length / 2
-  
+
   x = merge_sort(ary[0..(mid-1)])
   y = merge_sort(ary[mid..-1])
-  
   return merge(x, y)
 end
   
 def merge(ary1, ary2)
   sorted = []
-  
   (ary1.length + ary2.length).times do
     if ary1[0] <= ary2[0]
       sorted << ary1.slice!(0)
       ary1 << Float::INFINITY if ary1.empty? #Setting this to infinity since, hey, it's a big number.
-    elsif ary2[0] <= ary1[0]
+    else
       sorted << ary2.slice!(0)
       ary2 << Float::INFINITY if ary2.empty?
     end
   end
-  
   return sorted
 end
 
